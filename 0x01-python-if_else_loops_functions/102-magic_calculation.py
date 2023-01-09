bd7873a20903bd4dac5f94631f950794814df931
Babatunde David <davidbabs19@yahoo.com>
2023-01-09 00:39:33 -0800

    Task 16

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -2,7 +2,7 @@
 def magic_claculation(a, b, c):
     if (a < b):
         return (c)
-    elif(c < b):
-        return (a+b)
+    elif(c > b):
+        return (a + b)
     else:
-        return (a*b-c)
+        return (a * b - c)
