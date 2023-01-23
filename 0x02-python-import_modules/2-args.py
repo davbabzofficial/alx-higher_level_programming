5319a1e688c94f46b790cdc695f2d4239508aaff
Babatunde David <davidbabs19@yahoo.com>
2023-01-23 08:04:33 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if size - 1 == 0:
         print("{} arguments.".format(size - 1))
     elif size - 1 == 1:
-        print("{} arugument:".format(size - 1))
+        print("{} argument:".format(size - 1))
         print("{}: {}".format(size - 1, sys.argv[1]))
     else:
         print("{} arguments:".format(size - 1))
         for idx in range(1, size):
-            iii print("{}: {}".format(idx, sys.argv[idx])
+            print("{}: {}".format(idx, sys.argv[idx]))
