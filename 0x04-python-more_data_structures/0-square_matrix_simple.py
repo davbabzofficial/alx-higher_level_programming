0b31a7b05074bb87e0f5b8ba11d578015c60de31
Babatunde David <davidbabs19@yahoo.com>
2023-01-23 19:40:39 -0800

    Tasks combined

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        new_copy = list(map(lambda x: x ** 2, i))
+        new_matrix.append(new_copy)
+    return new_matrix
